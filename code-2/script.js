e986d4538e84100e8d8ae30559ce56098338cdb1
veljkoza <veljko.sekulic1998@gmail.com>
2020-04-10 02:52:27 +0200

    Everything is being loaded from api

--- code-2/script.js
+++ code-2/script.js
@@ -1,8 +1,10 @@
+const gridContainer = document.querySelector('.grid-container');
 const data = [];
 
 let count = 70;
 for (let i = 0; i < count; i++) {
-	fetchOneGame(i);	
+  fetchOneGame(i);
+
 }
 
 console.log(data);
@@ -19,24 +21,16 @@ function fetchOneGame(id) {
       return response.json();
     })
     .then((obj) => {
-      let newObj = {
-        id: 0,
-        bgImg: "",
-        name: "",
-        publisher: "",
-        genre: [],
-        rating: 0,
-        url: "",
-      };
+      let newObj = { genre: [] };
+
       newObj.id = obj.id;
       newObj.bgImg = obj.background_image;
       newObj.name = obj.name;
       if (obj.publishers === undefined) {
-        newObj.publisher = "not known";
-	  }else if(obj.publishers[0] === undefined){
-        newObj.publisher = obj.publishers[0].name;
-	  }
-	   else {
+        newObj.publisher = 'not known';
+      } else if (obj.publishers[0] === undefined) {
+        newObj.publisher = 'not known';
+      } else {
         newObj.publisher = obj.publishers[0].name;
       }
       newObj.rating = obj.rating_top;
@@ -45,10 +39,79 @@ function fetchOneGame(id) {
       [...obj.genres].forEach((genre) => {
         newObj.genre.push(genre.name);
       });
-
       data.push(newObj);
+      let newGameDiv = createGameDiv(newObj);
+      addGame(newGameDiv);
     })
     .catch((err) => {
       console.log(err);
     });
+}
+
+
+function addGame(gameDiv){
+  gridContainer.appendChild(gameDiv);
+}
+
+function createGameDiv(gameObj){
+  let gameName = gameObj.name;
+  let gamePublisher = gameObj.publisher;
+  let gameGenres = gameObj.genre;
+  let gameRating = gameObj.rating;
+  let gameBg = gameObj.bgImg;
+  let gameCard = document.createElement('div');
+  gameCard.classList.add('game-card');
+
+  let gameInfo = document.createElement('div');
+  gameInfo.classList.add('game-info');
+
+  let showName = document.createElement('h4');
+  showName.innerHTML = gameName;
+
+  let showPublisher = document.createElement('h4');
+  showPublisher.innerHTML = gamePublisher;
+
+  let showGenre = document.createElement('p');
+  gameGenres.forEach(genre => {
+    showGenre.innerHTML += `|${genre}|`
+  })
+
+  let stars = addStars(gameRating);
+
+  gameInfo.appendChild(showName);
+  gameInfo.appendChild(showPublisher);
+  gameInfo.appendChild(showGenre)
+  gameInfo.appendChild(stars);
+
+  addBackgroundImg(gameBg);
+  gameCard.appendChild(gameInfo);
+
+  return gameCard;
+
+  function addStars(rating){
+    let starsDiv = document.createElement('div');
+    starsDiv.classList.add('stars');
+
+    let counter = 0;
+    for (let i = 0; i < 5; i++) {
+      let star = document.createElement('i');
+      star.classList.add('fas','fa-star');
+      if(counter<rating){
+        star.classList.add('yellow');
+        counter++;
+      }   
+      starsDiv.appendChild(star);
+    }
+
+    return starsDiv;
+  }
+
+  function addBackgroundImg(url){
+    gameCard.style.background = `url(${url})`;
+    gameCard.style.backgroundRepeat = 'no-repeat';
+    gameCard.style.backgroundSize= 'cover';
+
+  }
+
+
 }
